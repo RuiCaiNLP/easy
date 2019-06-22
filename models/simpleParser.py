41d1ca4d8887666393351c5928608a1fdb8a01fd
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-06-22 23:06:02 +0100

    train file

--- models/simpleParser.py
+++ models/simpleParser.py
@@ -188,7 +188,7 @@ class simpleParser(nn.Module):
 
         uniScores_arg = uniScores_arg.view(batch_size, seq_len, 1).expand(-1, -1, self._vocab.rel_size)
         uniScores_arg_selected = uniScores_arg.index_select(0, torch.tensor(sample_indices_selected).to(device))
-        rel_logits += uniScores_pred_selected# + uniScores_pred_selected
+        rel_logits = rel_logits + uniScores_arg_selected + uniScores_pred_selected
 
         ##enforce the score of null to be 0
         rel_logits[:,:, 42] = torch.zeros(total_preds_num, seq_len).to(device)
